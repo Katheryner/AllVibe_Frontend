e1ba4c42399222efb10812f6b22f7948bfcdafc2
gelhg08 <angelicamhg31@gmail.com>
2024-05-16 15:31:57 -0500

    add fetch method

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
+const URL = "";
+const username = document.querySelector("");
+const email = document.querySelector("");
+const password = document.querySelector("");
+
 setInterval(nextSlide, 3000);
 
 function nextSlide() {
-  let activeSlide = document.querySelector('.carousel-item.active');
-  activeSlide.classList.remove('active');
+  let activeSlide = document.querySelector(".carousel-item.active");
+  activeSlide.classList.remove("active");
   let nextSlide = activeSlide.nextElementSibling;
   if (!nextSlide) {
-    nextSlide = document.querySelector('.carousel-inner .carousel-item');
+    nextSlide = document.querySelector(".carousel-inner .carousel-item");
   }
-  nextSlide.classList.add('active');
+  nextSlide.classList.add("active");
 }
 
+function registerUser() {
+  fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    }),
+  });
+}
