1c1431fd0817bcbaa3c2342487af3849773d477a
katheryner <katherinenrr123@gmail.com>
2024-05-17 10:49:19 -0500

    news form

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-const URL = "";
-const username = document.querySelector("");
-const email = document.querySelector("");
-const password = document.querySelector("");
-
 setInterval(nextSlide, 3000);
 
 function nextSlide() {
@@ -13,18 +8,4 @@ function nextSlide() {
     nextSlide = document.querySelector(".carousel-inner .carousel-item");
   }
   nextSlide.classList.add("active");
-}
-
-function registerUser() {
-  fetch(URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: username.value,
-      email: email.value,
-      password: password.value,
-    }),
-  });
 }
