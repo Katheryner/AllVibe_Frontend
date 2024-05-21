07a28fb2355f81fec0c9822b1828eba9ea4d0d03
william-esteban <114128992+william-esteban@users.noreply.github.com>
2024-05-21 11:12:41 -0500

    btn proposal

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const adminLink = document.getElementById('admin-link');
+    const proposalButton = document.getElementById('proposal-button');
   
     // Obtener el token del local storage
     const token = localStorage.getItem('token');
@@ -13,12 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
         if (userRole !== 'admin') {
           adminLink.style.display = 'none';
         }
+        if (userRole !== null ){
+          proposalButton.style.display = 'inline-block';
+
+        }
       } catch (error) {
         console.error('Error decoding token:', error);
         adminLink.style.display = 'none';
+        proposalButton.style.display = 'none';
       }
     } else {
       adminLink.style.display = 'none';
+      proposalButton.style.display = 'none';
     }
   });
   
